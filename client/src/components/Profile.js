98b54c5c3b07ac9cab8e199c3f26b84c04ff4ca9
jacquelineb <jacquelinesotheremail@gmail.com>
2021-09-28 01:18:45 -0700

    Broke down Profile into ProfileContentLeft and ProfileContentRight, which contain the left and right side content of a profile, respectively

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,6 +6,7 @@ import RecentGamesSection from './RecentGamesSection';
 import SteamcommunityUrl from './SteamcommunityUrl';
 import Blurbs from './Blurbs';
 import UserLinks from './UserLinks';
+import UserGroups from './UserGroups';
 import styles from '../styles/Profile.module.scss';
 
 function Profile() {
@@ -39,29 +40,47 @@ function Profile() {
     return <h1>you should redirect to a 404 page or something</h1>;
   }
 
-  console.log(userSummary.visibilityState);
-
   return (
     <div className={styles.profileContainer}>
-      <div className={styles.profileContentLeft}>
-        <ProfileSummary userSummary={userSummary} />
-        <SteamcommunityUrl url={userSummary.url} />
-        <UserLinks baseUrl={userSummary.url} />
-      </div>
-      <div className={styles.profileContentRight}>
-        <ProfileStatus
-          profileVisibility={userSummary.visibilityState}
-          userStatus={userSummary.personaState}
-          gameStatus={userSummary.gameExtraInfo}
-          username={userSummary.nickname}
-        />
-        {userSummary.visibilityState === 3 ? (
-          <>
-            <RecentGamesSection steamUserId={userSummary.steamID} />
-            <Blurbs username={userSummary.nickname} />
-          </>
-        ) : null}
-      </div>
+      <ProfileContentLeft userSummary={userSummary} />
+      <ProfileContentRight userSummary={userSummary} />
+    </div>
+  );
+}
+
+function ProfileContentLeft({ userSummary }) {
+  return (
+    <div className={styles.profileContentLeft}>
+      <ProfileSummary userSummary={userSummary} />
+      <SteamcommunityUrl url={userSummary.url} />
+      {userSummary.visibilityState === 3 ? (
+        <>
+          <UserLinks baseUrl={userSummary.url} />
+          <UserGroups
+            steamUserId={userSummary.steamID}
+            primaryGroupId={userSummary.primaryGroupID}
+          />
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+function ProfileContentRight({ userSummary }) {
+  return (
+    <div className={styles.profileContentRight}>
+      <ProfileStatus
+        profileVisibility={userSummary.visibilityState}
+        userStatus={userSummary.personaState}
+        gameStatus={userSummary.gameExtraInfo}
+        username={userSummary.nickname}
+      />
+      {userSummary.visibilityState === 3 ? (
+        <>
+          <RecentGamesSection steamUserId={userSummary.steamID} />
+          <Blurbs username={userSummary.nickname} />
+        </>
+      ) : null}
     </div>
   );
 }
